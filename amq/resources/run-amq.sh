e8054b561534d5f13de8e196279ea60233d972d8
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2018-05-25 18:32:54 +0300

    remove one of slf4j bindings

--- amq/resources/run-amq.sh
+++ amq/resources/run-amq.sh
@@ -8,5 +8,7 @@ for f in bootstrap.xml broker.xml logging.properties ; do
 	envsubst < $AMQ_HOME/conf/$f > /var/run/amq/$HOSTNAME/etc/$f
 done
 
+find /var/run/amq -name slf4j-log4j12-1.7.22.redhat-1.jar -exec rm -f {} \;
+
 exec /var/run/amq/$HOSTNAME/bin/artemis run
 
