47690fc3e12ec2dd87dccbb30cd0f63eeccc6d53
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2018-05-25 19:47:11 +0300

    jolokia access control

--- amq/resources/run-amq.sh
+++ amq/resources/run-amq.sh
@@ -12,9 +12,8 @@ $AMQ_HOME/bin/artemis create \
 LOGGING_LEVEL=${LOGGING_LEVEL:-INFO}
 
 sed -ci.bak1 "s|\.level=INFO|.level=$LOGGING_LEVEL|g" /var/run/amq/$HOSTNAME/etc/logging.properties
-sed -ci.bak1 '\
-    /<restrict/a <remote><host>10.128.0.0/14</host></remote> ; \
-    /<\/restrict/a <allow-origin>http://*.openshift.tk</allow-origin> <allow-origin>http://amq*:*/*</allow-origin> ; \
-    ' /var/run/amq/$HOSTNAME/etc/jolokia-access.xml
+sed -ci.bak1 \
+    '/<restrict>/a <remote><host>10.128.0.0/14</host></remote> ; /<\/restrict/a <allow-origin>http://*.openshift.tk</allow-origin> <allow-origin>http://amq*:*/*</allow-origin>' 
+    /var/run/amq/$HOSTNAME/etc/jolokia-access.xml
 
 exec /var/run/amq/$HOSTNAME/bin/artemis run
