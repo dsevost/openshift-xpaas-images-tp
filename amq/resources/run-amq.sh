ef3842dae13c815723cfd5e8ef6e58735838334f
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2018-05-25 19:06:03 +0300

    arranging to AMQ 7.1

--- amq/resources/run-amq.sh
+++ amq/resources/run-amq.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-$AMQ_HOME/bin/artemis create --user ${ADMIN_USER:-admin} --password ${ADMIN_PASSWORD:-admin} --allow-anonymous /var/run/amq/$HOSTNAME
+$AMQ_HOME/bin/artemis create \
+    --aio \
+    --
+    --user ${ADMIN_USER:-admin} \
+    --password ${ADMIN_PASSWORD:-admin} \
+    --allow-anonymous \
+    --verbose \
+    /var/run/amq/$HOSTNAME
 
 LOGGING_LEVEL=${LOGGING_LEVEL:-INFO}
 
-for f in bootstrap.xml broker.xml logging.properties ; do 
-	envsubst < $AMQ_HOME/conf/$f > /var/run/amq/$HOSTNAME/etc/$f
-done
+sed -ci.bak1 /var/run/amq/$HOSTNAME/etc/logging.properties "s|\.level=INFO|.level=$LOGGING_LEVEL|g"
 
-find /var/run/amq -name slf4j-log4j12-1.7.22.redhat-1.jar -exec rm -f {} \;
+#for f in bootstrap.xml broker.xml logging.properties ; do 
+#	envsubst < $AMQ_HOME/conf/$f > /var/run/amq/$HOSTNAME/etc/$f
+#done
+
+#find /var/run/amq -name slf4j-log4j12-1.7.22.redhat-1.jar -exec rm -f {} \;
 
 exec /var/run/amq/$HOSTNAME/bin/artemis run
-
