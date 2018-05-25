a6af66016f81c710b20bef29f05f55c2b322c701
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2018-05-25 20:05:00 +0300

    jolokia bind all addresses

--- amq/resources/run-amq.sh
+++ amq/resources/run-amq.sh
@@ -14,14 +14,14 @@ LOGGING_LEVEL=${LOGGING_LEVEL:-INFO}
 sed -ci.bak1 "s|\.level=INFO|.level=$LOGGING_LEVEL|g" /var/run/amq/$HOSTNAME/etc/logging.properties
 
 sed -ci.bak1 \
-    '/<restrict>/a <remote><host>10.128.0.0/14</host></remote>' \
+    '/<restrict>/a     <remote><host>10.128.0.0/14</host></remote>' \
     /var/run/amq/$HOSTNAME/etc/jolokia-access.xml
 sed -ci.bak1 \
-    '/<cors>/a <allow-origin>http://*.openshift.tk</allow-origin> <allow-origin>http://amq*:*/*</allow-origin>' \
+    '/<cors>/a         <allow-origin>http://*.openshift.tk</allow-origin> <allow-origin>http://amq*:*/*</allow-origin>' \
     /var/run/amq/$HOSTNAME/etc/jolokia-access.xml
 
 sed -ci.bak1 \
-    '|http://localhost:8161|http://0.0.0.0:8161|' \
+    's|http://localhost:8161|http://0.0.0.0:8161|' \
     /var/run/amq/$HOSTNAME/etc/bootstrap.xml
 
 exec /var/run/amq/$HOSTNAME/bin/artemis run
