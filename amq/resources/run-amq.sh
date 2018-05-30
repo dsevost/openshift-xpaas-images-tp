9253b28a4de0a545b60617ed6a18940e29755037
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2018-05-30 13:37:42 +0300

    replicated configuration added

--- amq/resources/run-amq.sh
+++ amq/resources/run-amq.sh
@@ -1,38 +1,69 @@
 #!/bin/bash
 
-set -x
+set -ex
+
+ADM_USER=${ADMIN_PASSWORD:-admin}
+ADM_PASSWORD=${ADMIN_USER:-admin}
+
+echo $HOSTNAME | grep '^[a-z0-9]\+-0$' && MASTER_or_SLAVE=master || MASTER_or_SLAVE=slave
+
+INSTANCE_HOME=/var/run/amq/broker
+
+[ -z "$AMQ_CLUSTERED" ] && \
+    CLUSTERED="\
+	--replicated \
+	--failover-on-shutdown \
+	--clustered \
+	--host $HOSTNAME \
+	--cluster-user $ADM_USER \
+	--cluster-password $ADM_PASSWORD \
+	--max-hops 1 \
+	--${MASTER_or_SLAVE} \
+    "
 
 $AMQ_HOME/bin/artemis create \
     --addresses=localhost,$HOSTNAME \
-    --user ${ADMIN_USER:-admin} \
-    --password ${ADMIN_PASSWORD:-admin} \
     --allow-anonymous \
+    --password ${ADM_USER} \
+    --role admin \
+    --user ${ADM_PASSWORD} \
     --verbose \
-    /var/run/amq/$HOSTNAME
+    $CLUSTERED \
+    $INSTANCE_HOME
 
 LOGGING_LEVEL=${LOGGING_LEVEL:-INFO}
 
-#sed -ci.bak1 "s|\.level=INFO|.level=$LOGGING_LEVEL|g" /var/run/amq/$HOSTNAME/etc/logging.properties
 sed -ci.bak1 "\
     s/^loggers=\(.*\)/loggers=\1,aio.prometheus.jmx/ ; \
     s/logger.handlers=.*/logger.handlers=CONSOLE/ ; \
     s/handler.FILE/#handler.FILE/ ; \
     s/\.level=INFO/.level=$LOGGING_LEVEL/g ; \
-    " /var/run/amq/$HOSTNAME/etc/logging.properties
+    " $INSTANCE_HOME/etc/logging.properties
 
-#    '/<cors>/a<allow-origin>http://*.apps.openshift.tk/*</allow-origin> <allow-origin>http://amq*:*/*</allow-origin>' \
-sed -ci.bak1 '\
-    s|<cors>|<cors>\n		<allow-origin>http://*.apps.openshift.tk/*</allow-origin>\n		<allow-origin>http://amq*:*/*</allow-origin>| ;\
-    ' /var/run/amq/$HOSTNAME/etc/jolokia-access.xml
+if [ -z "${AMQ_CONSOLE_PUBLIC_URL}" ] ; then
+    sed -ci.bak1 \
+	"s|<cors>|<cors>\n		<allow-origin>*amq-*</allow-origin>\n| ;" \
+	$INSTANCE_HOME/etc/jolokia-access.xml
+else
+    sed -ci.bak1 \
+	"s|<cors>|<cors>\n		<allow-origin>${AMQ_CONSOLE_PUBLIC_URL}</allow-origin>\n| ;" \
+	$INSTANCE_HOME/etc/jolokia-access.xml
+fi
 
 sed -ci.bak1 \
     's|http://localhost:8161|http://0.0.0.0:8161|' \
-    /var/run/amq/$HOSTNAME/etc/bootstrap.xml
+    $INSTANCE_HOME/etc/bootstrap.xml
 
 sed -ci.bak1 \
     '/JAVA_ARGS \\/a $JAVA_OPTS_APPEND \\' \
-    /var/run/amq/$HOSTNAME/bin/artemis
+    $INSTANCE_HOME/bin/artemis
 
-#cp -f $AMQ_HOME/templates/management.xml /var/run/amq/$HOSTNAME/etc
+sed -ci.bak1 "\
+    s/<master\/>/<master>\n		<check-for-live-server>true<\/check-for-live-server>\n		<\/master>/ ; \
+    s/<slave\/>/<slave>\n		<allow-failback>true<\/allow-failback>\n		<\/slave>/ ; \
+    /<broadcast-groups>/,/<\/discovery-groups>/d ; \
+    s/<\/connector>/<\/connector>\n<connector name=\"discovery-connector\">tcp:\/\/${HOSTNAME}:61616<\/connector>/ ; \
+    s/<discovery-group-ref discovery-group-name=\"dg-group1\"\/>/<static-connectors>\n		<connector-ref>discovery-connector<\/connector-ref>\n		<\/static-connectors>/ ; \
+    " $INSTANCE_HOME/etc/broker.xml
 
-exec /var/run/amq/$HOSTNAME/bin/artemis run
+exec $INSTANCE_HOME/bin/artemis run
