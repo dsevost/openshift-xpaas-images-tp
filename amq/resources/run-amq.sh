91783ad3c04859a1a14657fd89fe55f6c5605d8b
afilippov <afilippov@emdev.ru>
2018-07-12 14:14:07 +0300

    Disable SSLv2Hello

--- amq/resources/run-amq.sh
+++ amq/resources/run-amq.sh
@@ -73,11 +73,7 @@ sed -ci.bak1 \
 
 sed -ci.bak1 \
     '/JAVA_ARGS \\/a $JAVA_OPTS_APPEND \\' \
-    $INSTANCE_HOME/bin/artemis
-
-sed -ci.bak1 \
-    's|jdk.tls.disabledAlgorithms=|jdk.tls.disabledAlgorithms=SSLv2Hello,|' \
-    /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.171-8.b10.el7_5.x86_64/jre/lib/security/java.security    
+    $INSTANCE_HOME/bin/artemis 
 
 sed -ci.bak1 "\
     s/<master\/>/<master>\n		<check-for-live-server>true<\/check-for-live-server>\n		<\/master>/ ; \
