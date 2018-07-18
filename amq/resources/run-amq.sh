54b72dd82f28c45b6f51ce8b448ec0f56d4cd1d0
afilippov <afilippov@emdev.ru>
2018-07-18 18:57:47 +0300

    SSL stuff for replicated

--- amq/resources/run-amq.sh
+++ amq/resources/run-amq.sh
@@ -92,7 +92,7 @@ sed -ci.bak2 "\
     " $INSTANCE_HOME/etc/broker.xml
 #In case of replicated
 sed -ci.bak2 "\
-    s|<acceptor name=\"amqp\">tcp:\/\/${CURRENT_HOST}:5672?tcpSendBufferSize=1048576;tcpReceiveBufferSize=1048576;protocols=AMQP;useEpoll=true;amqpCredits=1000;amqpMinCredits=300<\/acceptor>|<acceptor name=\"amqp\">tcp:\/\/${CURRENT_HOST}:5672?tcpSendBufferSize=1048576;tcpReceiveBufferSize=1048576;protocols=AMQP;useEpoll=true;amqpCredits=1000;amqpMinCredits=300<\/acceptor>\n       <acceptor name=\"amqps\">tcp:\/\/${CURRENT_HOST}:5673?tcpSendBufferSize=1048576;tcpReceiveBufferSize=1048576;protocols=AMQP;useEpoll=true;amqpCredits=1000;amqpMinCredits=300;sslEnabled=true;keyStorePath=/var/run/secrets/amq/keystores/amq-broker.ks;keyStorePassword=$JKS_PASSWORD<\/acceptor>| \
+    s|<acceptor name=\"amqp\">tcp:\/\/${CURRENT_HOST}:5672?tcpSendBufferSize=1048576;tcpReceiveBufferSize=1048576;protocols=AMQP;useEpoll=true;amqpCredits=1000;amqpMinCredits=300<\/acceptor>|<acceptor name=\"amqp\">tcp:\/\/${CURRENT_HOST}:5672?tcpSendBufferSize=1048576;tcpReceiveBufferSize=1048576;protocols=AMQP;useEpoll=true;amqpCredits=1000;amqpMinCredits=300<\/acceptor>\n       <acceptor name=\"amqps\">tcp:\/\/${CURRENT_HOST}:5673?tcpSendBufferSize=1048576;tcpReceiveBufferSize=1048576;protocols=AMQP;useEpoll=true;amqpCredits=1000;amqpMinCredits=300;sslEnabled=true;keyStorePath=/var/run/secrets/amq/keystores/${CURRENT_HOST}-broker.ks;keyStorePassword=$JKS_PASSWORD<\/acceptor>| \
     " $INSTANCE_HOME/etc/broker.xml    
 
 sed -ci.bak1 "\
